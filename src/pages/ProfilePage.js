13f5e11c973944bb8329542487830981c739338b
bengallagher <bgallag5@gmail.com>
2021-10-18 20:51:31 -0400

    init

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class ProfilePage extends Component {
+
+
+    //access user data via state or Context?
+
+
+
+    render() {
+
+        //render user's page: user info, pokemon, currentTeam, avatar?
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
