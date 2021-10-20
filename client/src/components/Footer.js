575f0563cc09c047fb9d3b8201b03f80ad295132
Emily N Mayeski <enmayeski@gmail.com>
2021-10-20 11:59:22 -0400

    basic CSS done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,18 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>Home</p>
-        <p>About PokeTraders</p>
-        <p className="footer-yellow"><span class="copyright">&#169;</span>2021 PocketTraders | Note that this is not a real site and we do not own Pokemon. This was done for a class project. Please do not sue us.</p>
+        <div className="columns">
+          <a className="column">Home</a>
+          <a className="column">About PokeTraders</a>
+        </div>
+        <p className="footer-yellow">
+          <span class="copyright">&#169;</span>2021 PocketTraders{" "}
+        </p>
+        <p className="footer-yellow">
+          {" "}
+          Note that this is not a real site and we do not own Pokemon. This was
+          done for a class project. Please do not sue us.
+        </p>
       </div>
     </footer>
   );
