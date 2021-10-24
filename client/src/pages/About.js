cec812d7f81cd0f88ec74c6610404a00db1d2aa8
Gaye Proctor <gayebroni@yahoo.com>
2021-10-24 12:21:19 -0400

    update layout of about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,68 +10,78 @@ const About = () => {
     
 
 <section className="hero is-fullheight">
-      <div className="hero-body columns container about-container">
-        
-    className="about-container">
 
-      
+<div className="tile is-parent">
+            <article className="tile is-child notification is-warning is- is-offset-8">
+              <p className="title">Wide tile</p>
+              {/* <p className="subtitle">Aligned with the right tile</p> */}
+              <p className="subtitle">With an image</p>
+                <figure className="image is-1by1">
+                  <img className="is-rounded" src="./images/Dana.png"  alt="photo of Dana" />
+                </figure>
+              <div className="content">Content </div>
+            </article>
+          </div> 
 
-      
-      
-
-        
-  
-
-      <div className="tile is-ancestor">
-        <div className="tile is-vertical is-8">
+      <div className="hero-body columns about-container">
+      {/* <div className="tile is-ancestor"> */}
+        <div className="tile is-vertical">
         {/* <p class="post-comment" contenteditable="false">${post.comment}</p> */}
-          <div className="tile">
+          {/* <div className="tile"> */}
             <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-info is">
+            {/* <article className="tile is-child notification is-info is">
                 <p className="title">Middle tile</p>
                 <p className="subtitle">With an image</p>
                 <figure className="image is-1by1">
                   <img src="./images/Dana.png" width="50" height="50"  alt="photo of Dana" />
                 </figure>
-              </article>
+              </article> */}
               
             </div>
-            <div className="tile is-parent m-6is large is-full">
-              <article className="tile is-child notification is-info ">
-                <p className="title">Middle tile</p>
-                <p className="subtitle">With an image</p>
-                <figure className="image is-1by1">
-                  <img src="./images/Dana.png" width="50" height="50"  alt="photo of Dana" />
-                </figure>
-              </article>
-
-              
-              
+            <div className="tile is-parent">
               <article className="tile is-child notification is-info">
                 <p className="title">Middle tile</p>
                 <p className="subtitle">With an image</p>
-                <figure className="image is-1by1">
-                  <img src="./images/Dana.png" width="50" height="50"  alt="photo of Dana" />
+                <figure className="image is-2by2">
+                  <img className="is-rounded" src="./images/Dana.png" alt="photo of Dana" />
                 </figure>
               </article>
+              &nbsp; 
 
               <article className="tile is-child notification is-info">
                 <p className="title">Middle tile</p>
                 <p className="subtitle">With an image</p>
-                <figure className="image is-1by1">
-                  <img src="./images/Dana.png" width="50" height="50"  alt="photo of Dana" />
+                <figure className="image is-2by2">
+                  <img className="is-rounded" src="./images/Dana.png"  alt="photo of Dana" />
                 </figure>
-              </article>
+              </article> 
+              &nbsp;
+              <article className="tile is-child notification is-info">
+                <p className="title">Middle tile</p>
+                <p className="subtitle">With an image</p>
+                <figure className="image is-2by2">
+                  <img className="is-rounded" src="./images/Dana.png"  alt="photo of Dana" />
+                </figure>
+              </article> 
+              &nbsp;
+              <article className="tile is-child notification is-info">
+                <p className="title">Middle tile</p>
+                <p className="subtitle">With an image</p>
+                <figure className="image is-2by2">
+                  <img className="is-rounded" src="./images/Dana.png"  alt="photo of Dana" />
+                </figure>
+              </article> 
+              
             </div>
           </div>
 
-          <article className="tile is-child notification is-info">
+          {/* <article className="tile is-child notification is-info">
                 <p className="title">Middle tile</p>
                 <p className="subtitle">With an image</p>
                 <figure className="image is-4by3">
                   <img src="client/public/images/Dana.png" />
                 </figure>
-              </article>
+              </article> */}
 
           {/* <div className="tile is-parent">
             <article className="tile is-child notification is-danger">
@@ -80,7 +90,7 @@ const About = () => {
               <div className="content">Content </div>
             </article>
           </div> */}
-        </div>
+        {/* </div> */}
         {/* <div className="tile is-parent">
         <article className="tile is-child notification is-info">
                 <p className="title">Middle tile</p>
@@ -90,7 +100,7 @@ const About = () => {
                 </figure>
               </article>
         </div> */}
-      </div>
+      {/* </div> */}
     </div>
 
     </section> 
