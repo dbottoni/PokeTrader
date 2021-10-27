faf7d31f91aaa19568741552d369f5abc906896a
Dana Bottoni <72705457+dbottoni@users.noreply.github.com>
2021-10-27 15:50:47 -0400

    Update resolvers.js

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -21,21 +21,7 @@ const resolvers = {
        },
         pokemons:async()=>{
           return Pokemon.find()
-        },
-        donate: async()=>{
-
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
-          line_items: [{
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          }],
-          mode: 'payment',
-          success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-          cancel_url: 'https://example.com/cancel',
-        });
-
-      }
+        }
     },
     Mutation:{
         addUser: async(parent, args) => {
