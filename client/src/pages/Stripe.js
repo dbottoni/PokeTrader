b2f8d28a61d1870b8d671e67198ec0b254567076
Dana Bottoni <danabottoni@gmail.com>
2021-10-27 13:53:41 -0400

    stripe changes

--- client/src/pages/Stripe.js
+++ client/src/pages/Stripe.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js"
 
+const stripePromise = loadStripe(
+          "pk_test_51Jjp0ZGSMAd7MIHGemJUH9wgAOsy2cAHV2xPwqpTZmTlJn3VxnNDlgPftS4eH7OIzrKZfIA0fIYf9NEJsRSIJKD100dHuxsupy"
+    );
 
-const Stripe = () => {
+  const Stripe = ({ itemID, amount }) => {
 
-  const stripePromise = loadStripe(
-        "pk_test_51Jjp0ZGSMAd7MIHGemJUH9wgAOsy2cAHV2xPwqpTZmTlJn3VxnNDlgPftS4eH7OIzrKZfIA0fIYf9NEJsRSIJKD100dHuxsupy"
-   );
 
-  const DonationButton = ({ itemID, amount }) => {
     const handleClick = async (event) => {
       const stripe = await stripePromise;
       stripe
@@ -27,27 +26,17 @@ const Stripe = () => {
     };
 
     return (
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
-        onClick={handleClick}
-      >
-        Donate '$'{amount}
-      </button>
+      <p>Donate to us</p>
+      // <button
+      //   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+      //   onClick={handleClick}
+      // >
+      //   Donate '$'{amount}
+      // </button>
     );
   };
-  // export default function App() {
-  //   return (
-  //     <>
-  //       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-  //         <DonationButton
-  //           amount={"5.00"}
-  //           itemID="price_1IUx1FJ2iOysJZvP1LD3EzTR"
-  //         ></DonationButton>
-  //       </div>
-  //     </>
-  //   );
-  // }
+ 
 
-}
+
 
 export default Stripe;
