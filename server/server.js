5d46e0cd79ea90e307d277d8a64b834281d5a2f2
bengallagher <bgallag5@gmail.com>
2021-10-27 13:57:01 -0400

    server change

--- server/server.js
+++ server/server.js
@@ -38,11 +38,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
-// app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
